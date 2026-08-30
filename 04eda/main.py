eeb9f76bb2b01bd0fc59710ffbb4b73365d8d10f
dgdata21 <110827425+dgdata21@users.noreply.github.com>
2026-08-30 07:53:06 +0300

    Update main.py

--- 04eda/main.py
+++ 04eda/main.py
@@ -2,6 +2,10 @@ import eda01_intro
 import eda02_general
 import eda03_general
 import eda04_general
+import eda05_manager01
+import eda05_manager02
+import eda05_manager03
+import eda05_manager04
 
 if __name__ == "__main__":
     eda01_intro.main()
@@ -13,4 +17,19 @@ if __name__ == "__main__":
     eda_clean, monthly_stats, period_bootstrap = eda04_general.main(
         eda_clean=eda_clean, monthly_stats=monthly_stats, verbose=True
     )
+
+    mngr_no_bonus, mngr_monthly, mngr_stat = eda05_manager01.main(verbose=True)
+    mngr_pareto_rev, mngr_pareto_mrg, gini_rev, gini_margin = (
+        eda05_manager02.main(mngr_monthly=mngr_monthly, verbose=True)
+    )
+    df_bootstrap, bootstrap_df = eda05_manager03.main(
+        mngr_no_bonus=mngr_no_bonus, verbose=True
+    )
+    manager_diagnoses = eda05_manager04.main(
+        bootstrap_df=bootstrap_df,
+        df_bootstrap=df_bootstrap,
+        mngr_no_bonus=mngr_no_bonus,
+        verbose=True,
+    )
+
     print()
